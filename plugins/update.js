01343ab29182d15c68996ad3fd0fdfade2ba4678
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-06-25 09:31:38 -0500

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, isROwner }) => {
 }
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = /^update$/i
+handler.command = /^actualizar$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
